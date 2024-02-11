b9dd70a5d05280b61f9d9615754b96195a5aaaa4
Atay <atajmamasadykov@gmail.com>
2024-02-11 23:51:50 +0600

    Code refactoring

--- src/components/BestDoctors/BestDoctors.jsx
+++ src/components/BestDoctors/BestDoctors.jsx
@@ -4,19 +4,34 @@ import styles from './BestDoctors.module.scss';
 const BestDoctors = () =>{
     console.log(DB[0].name)
     return(
-         <div className={styles.bestDoctors}>
+         <>
+
             <h2>Лучшие <span> врачи </span></h2>
-            <p className={styles.title}>Попасть в команду медицинской клиники «MedCheck» могут
-               только лучшие специалисты с многолетней практикой и доказанным опытом.</p>
-            <div className={styles.doctorsData}>{DB.map((e, i)=>(
+
+            <p className={styles.title}>
+               Попасть в команду медицинской клиники «MedCheck» могут
+               только лучшие специалисты с многолетней практикой и доказанным опытом.
+            </p>
+
+            <div className={styles.doctorsData} > { DB.map( (e, i) => (
+
                 <div className={styles.cards} key={i}>
-                <div><img width="223px" src={e.img} alt="" /></div>
-                <p>{e.prof}</p>
-                <p className={styles.prof}>{e.name}</p></div>
-            ))}</div>
+                    <div>
+                        <img src={e.img} alt="photo" />
+                    </div>
+
+                    <p>{e.name}</p>
+                    <p className={styles.prof}>{e.prof}</p>
+                </div>
+
+            ))}
+            </div>
+
+
             <div className={styles.btn}>
-            <button>Все врачи клиники</button></div>
-        </div>
+                <button>Все врачи клиники</button>
+            </div>
+        </>
     )
 }
 export default BestDoctors;
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -170,34 +170,42 @@ const Homepage = () =>{
 
             </div>
 
-            <div className={styles.bestDoctors}>
+            <div className={styles.bestDoctorsSection}>
                 <BestDoctors/>     
             </div>
-            <div className={styles.form}>
-            <div className={styles.forms}>
-                <h1>Оставьте заявку</h1>
-                <p>Оставьте свой номер и наши специалисты свяжутся с Вами в ближайшее время</p>
-                <div className={styles.inputs}>
-                    <div className={styles.input1}>
-                        <div className={styles.text}>Как к Вам обратиться?</div>
-                        <input type="text" placeholder='Введите имя' />
-                        <img src={user} alt="user"/>
-                    </div>
-                    <div className={styles.input2}>
-                        <div className={styles.text}>Номер мобильного телефона</div>
-                       <div>
-                        <input  type="text" placeholder='+996 (___) __-__-__' />
-                        <img src={phone2} alt="phone" />
-                    </div>
-                    </div>
-                   
-                </div>
 
-                <div className={styles.buttonCont}><button>ОТПРАВИТЬ ЗАЯВКУ</button><span><img src={send} alt="" /></span></div>
-            </div>
-            <div className={styles.images}>
-                <img src={form} alt="" />
-            </div>
+            <div className={styles.formSection}>
+
+
+                    <div className={styles.form}>
+
+                        
+                        <h1>Оставьте заявку</h1>
+
+                        <p>Оставьте свой номер и наши специалисты свяжутся с Вами в ближайшее время</p>
+
+                        <div className={styles.inputs}>
+                            <div className={styles.inputPanel}>
+                                <div className={styles.text}>Как к Вам обратиться?</div>
+                                <input type="text" placeholder='Введите имя' />
+                                <img src={user} alt="user"/>
+                            </div>
+
+                            <div className={styles.inputPanel}>
+                                <div className={styles.text}>Номер мобильного телефона</div>
+                                <input type="text" placeholder='+996 (___) __-__-__' />
+                                <img src={phone2} alt="user"/>
+                            </div>
+                        
+                        </div>
+
+
+                        <div className={styles.buttonCont}><button>ОТПРАВИТЬ ЗАЯВКУ</button><span><img src={send} alt="" /></span></div>
+                    </div>
+
+                    <div className={styles.images}>
+                        <img src={form} alt="Woman" />
+                    </div>
         </div>
 
 
