7e8b796b7d4edf2758345524d95b2748e13fcb4e
Atay <atajmamasadykov@gmail.com>
2024-04-06 23:49:22 +0600

    changes: 06.04.2024

--- src/utils/regex.js
+++
@@ -1,5 +0,0 @@
-export const PWD_REGEX = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
-export const USER_REGEX = /^[a-zA-Zа-яА-Я]+(?:[\s.]+[a-zA-Zа-яА-Я]+)*$/;
-export const EMAIL_REGEX = /^[^\s@.,?!-][^\s@]*@[^\s@]+\.[^\s@]+$/
-export const PHONE_REGEX = /^\+996\s?\d{3}\s?\d{3}\s?\d{3}$/
-
